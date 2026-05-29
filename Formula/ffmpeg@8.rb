b3b6f4e7bdb5800e070f3edd18a3a5a0899c644e
Fernando Agnes <f5011@miro.com>
2026-05-28 21:08:36 -0300

    ffmpeg 8

---
+++ Formula/ffmpeg@8.rb
@@ -0,0 +1,122 @@
+class Ffmpeg < Formula
+  desc "Play, record, convert, and stream select audio and video codecs"
+  homepage "https://ffmpeg.org/"
+  url "https://ffmpeg.org/releases/ffmpeg-8.0.1.tar.xz"
+  sha256 "05ee0b03119b45c0bdb4df654b96802e909e0a752f72e4fe3794f487229e5a41"
+  # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
+  # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
+  license "GPL-2.0-or-later"
+  revision 4
+  compatibility_version 1
+  head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
+
+  livecheck do
+    url "https://ffmpeg.org/download.html"
+    regex(/href=.*?ffmpeg[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 arm64_tahoe:   "d05cc234b10c73b7110258d44522152596e45d2c596f834d7628e9567e72b38f"
+    sha256 arm64_sequoia: "e20fcc85982e1ec4dfa073795e605a54b78804109730e64db596947346e46f22"
+    sha256 arm64_sonoma:  "e5606c884236ca8c1a942faaa7e62955a60e85de07df23d4f2c698061a67c9b4"
+    sha256 sonoma:        "878964f8311386f07a719e10fb77533bd0e9cb6d816ca76c283f9a5c1b5b705f"
+    sha256 arm64_linux:   "f5a701c93d3cb1c8c7f7d0f28c4aa44252604214b01b44654d74ae44c6baf0bd"
+    sha256 x86_64_linux:  "7db34826b4b98eb9f3bb1f5e758ee953f6ab36dc4bb97922fb3d1673b7af7bfc"
+  end
+
+  depends_on "pkgconf" => :build
+
+  # Only add dependencies required for dependents in homebrew-core
+  # or INCREDIBLY widely used and light codecs in the current year (2026).
+  # Add other dependencies to ffmpeg-full formula or consider making
+  # formulae dependent on ffmpeg-full.
+  # We should expect to remove e.g. x264 eventually (>=2027) when usage of it is
+  # negligible and has all moved to e.g. x265 instead.
+  depends_on "dav1d"
+  depends_on "lame"
+  depends_on "libvpx"
+  depends_on "openssl@3"
+  depends_on "opus"
+  depends_on "sdl2"
+  depends_on "svt-av1"
+  depends_on "x264"
+  depends_on "x265"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
+
+  on_linux do
+    depends_on "alsa-lib"
+    depends_on "libxcb"
+    depends_on "xz"
+    depends_on "zlib-ng-compat"
+  end
+
+  on_intel do
+    depends_on "nasm" => :build
+  end
+
+  # Fix for QtWebEngine, do not remove
+  # https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=270209
+  patch do
+    url "https://gitlab.archlinux.org/archlinux/packaging/packages/ffmpeg/-/raw/5670ccd86d3b816f49ebc18cab878125eca2f81f/add-av_stream_get_first_dts-for-chromium.patch"
+    sha256 "57e26caced5a1382cb639235f9555fc50e45e7bf8333f7c9ae3d49b3241d3f77"
+  end
+
+  # Add svt-av1 4.x support
+  patch do
+    url "https://git.ffmpeg.org/gitweb/ffmpeg.git/patch/a5d4c398b411a00ac09d8fe3b66117222323844c"
+    sha256 "1dbbc1a4cf9834b3902236abc27fefe982da03a14bcaa89fb90c7c8bd10a1664"
+  end
+
+  def install
+    # The new linker leads to duplicate symbol issue https://github.com/homebrew-ffmpeg/homebrew-ffmpeg/issues/140
+    ENV.append "LDFLAGS", "-Wl,-ld_classic" if DevelopmentTools.ld64_version.between?("1015.7", "1022.1")
+
+    # Fine adding any new options that don't add dependencies to the formula.
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --cc=#{ENV.cc}
+      --host-cflags=#{ENV.cflags}
+      --host-ldflags=#{ENV.ldflags}
+      --enable-ffplay
+      --enable-gpl
+      --enable-libsvtav1
+      --enable-libopus
+      --enable-libx264
+      --enable-libmp3lame
+      --enable-libdav1d
+      --enable-libvpx
+      --enable-libx265
+      --enable-openssl
+    ]
+
+    # Needs corefoundation, coremedia, corevideo
+    args += %w[--enable-videotoolbox --enable-audiotoolbox] if OS.mac?
+    args << "--enable-neon" if Hardware::CPU.arm?
+
+    system "./configure", *args
+    system "make", "install"
+
+    # Build and install additional FFmpeg tools
+    system "make", "alltools"
+    bin.install (buildpath/"tools").children.select { |f| f.file? && f.executable? }
+    pkgshare.install buildpath/"tools/python"
+  end
+
+  def caveats
+    <<~EOS
+      ffmpeg-full includes additional tools and libraries that are not included in the regular ffmpeg formula.
+    EOS
+  end
+
+  test do
+    # Create a 5 second test MP4
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=5", mp4out
+    assert_path_exists mp4out, "Failed to create video.mp4!"
+  end
+end
