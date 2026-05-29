92b391e509d70d493efe8d9ca2d0c7340bddea65
Fernando Agnes <f5011@miro.com>
2026-05-28 21:53:49 -0300

    Fixing deps

--- Formula/ffmpeg@8.rb
+++ Formula/ffmpeg@8.rb
@@ -6,7 +6,7 @@ class FfmpegAT8 < Formula
   # None of these parts are used by default, you have to explicitly pass `--enable-gpl`
   # to configure to activate them. In this case, FFmpeg's license changes to GPL v2+.
   license "GPL-2.0-or-later"
-  revision 4
+  revision 5
   compatibility_version 1
   head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
 
@@ -41,6 +41,9 @@ class FfmpegAT8 < Formula
     depends_on "libxcb"
     depends_on "xz"
     depends_on "zlib-ng-compat"
+    depends_on "fontconfig"
+    depends_on "freetype"
+    depends_on "harfbuzz"
   end
 
   on_intel do
